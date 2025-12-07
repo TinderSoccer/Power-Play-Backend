c3ea08e919e24a4408d809014bd67e938b689a87
jsilva <jsilva@github.com>
2025-12-07 01:07:59 -0300

    Expose order id in JSON

--- src/models/Order.js
+++ src/models/Order.js
@@ -33,4 +33,13 @@ const orderSchema = new mongoose.Schema({
   timestamps: true
 })
 
+orderSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+  }
+})
+
 export const Order = mongoose.model('Order', orderSchema)
